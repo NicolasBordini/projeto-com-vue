12d5624c55dac1b5b68749da34355e3d4181af7e
NicolasBordini <161318085+NicolasBordini@users.noreply.github.com>
2024-08-03 11:05:38 -0300

    Add files via upload

---
+++ VideoUnico.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h1>Vídeo Único</h1>
+    <!-- Conteúdo do componente -->
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'VideoUnico'
+}
+</script>
+
+<style scoped>
+/* Seus estilos aqui */
+</style>
